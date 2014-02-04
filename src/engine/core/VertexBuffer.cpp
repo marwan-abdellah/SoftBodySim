a4dff2057c8cb7a2a64f7e4356b2c43b4599d562
Lukasz Stanislawski <lukasz.stanislawski@gmail.com>
2014-02-04 16:38:30 +0100

    VertexBuffer initial implementation added.

---
+++ src/engine/core/VertexBuffer.cpp
@@ -0,0 +1,75 @@
+#include "VertexBuffer.h"
+#include <cstring>
+
+using namespace glm;
+
+
+GLVertexBuffer::GLVertexBuffer(unsigned int size) :
+	VertexBuffer(size, OPENGL_BUFFER)
+{
+	memset(mVBO, 0x0, sizeof(GLint) * VERTEX_ATTR_LAST);
+}
+
+void GLVertexBuffer::setVertexes(glm::vec3 *data)
+{
+	if (mVBO[VERTEX_ATTR_POSITION] == 0)
+		glGenBuffers(1, &mVBO[VERTEX_ATTR_POSITION]);
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO[VERTEX_ATTR_POSITION]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * mVertexesCount, data, GL_DYNAMIC_DRAW);
+}
+
+void GLVertexBuffer::setNormals(glm::vec3 *data)
+{
+	if (mVBO[VERTEX_ATTR_NORMAL] == 0)
+		glGenBuffers(1, &mVBO[VERTEX_ATTR_NORMAL]);
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO[VERTEX_ATTR_NORMAL]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * mVertexesCount, data, GL_DYNAMIC_DRAW);
+}
+
+void GLVertexBuffer::setTextCoords(glm::uvec2 *data)
+{
+	if (mVBO[VERTEX_ATTR_TEX_COORDS] == 0)
+		glGenBuffers(1, &mVBO[VERTEX_ATTR_TEX_COORDS]);
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO[VERTEX_ATTR_TEX_COORDS]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(uvec2) * mVertexesCount, data, GL_DYNAMIC_DRAW);
+}
+
+void GLVertexBuffer::setColors(glm::vec3 *data)
+{
+	if (mVBO[VERTEX_ATTR_COLOR] == 0)
+		glGenBuffers(1, &mVBO[VERTEX_ATTR_COLOR]);
+	glBindBuffer(GL_ARRAY_BUFFER, mVBO[VERTEX_ATTR_COLOR]);
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vec3) * mVertexesCount, data, GL_DYNAMIC_DRAW);
+}
+
+bool GLVertexBuffer::bind(VertexAttribute attr)
+{
+	if (!mVBO[attr]) return false;
+
+	switch(attr) {
+		case VERTEX_ATTR_POSITION:
+		case VERTEX_ATTR_NORMAL:
+		case VERTEX_ATTR_COLOR:
+			glEnableVertexAttribArray(attr);
+			glBindBuffer(GL_ARRAY_BUFFER, mVBO[attr]);
+			glVertexAttribPointer(attr, 3, GL_FLOAT, GL_FALSE, 0, 0);
+			break;
+		case VERTEX_ATTR_TEX_COORDS:
+			glEnableVertexAttribArray(attr);
+			glBindBuffer(GL_ARRAY_BUFFER, mVBO[attr]);
+			glVertexAttribPointer(attr, 2, GL_FLOAT, GL_FALSE, 0, 0);
+			break;
+		default:
+			return false;
+	};
+	return true;
+}
+
+void GLVertexBuffer::unbind(void)
+{
+	glBindVertexArray(0);
+	glDisableVertexAttribArray(VERTEX_ATTR_POSITION);
+	glDisableVertexAttribArray(VERTEX_ATTR_NORMAL);
+	glDisableVertexAttribArray(VERTEX_ATTR_COLOR);
+	glDisableVertexAttribArray(VERTEX_ATTR_TEX_COORDS);
+}
--- src/engine/core/VertexBuffer.h
+++ src/engine/core/VertexBuffer.h
@@ -1,26 +1,51 @@
 #ifndef __VERTEX_BUFFER_H
 #define __VERTEX_BUFFER_H
 
+#include <glm/glm.hpp>
+
+#define GL_GLEXT_PROTOTYPES
+#include <GL/gl.h>
 
 class VertexBuffer {
 public:
-	enum {
+	enum VertexBufferType {
 		CPU_BUFFER,
 		OPENGL_BUFFER,
-	} type;
+	};
+
+	VertexBuffer(unsigned int s, VertexBufferType type = CPU_BUFFER) : m_type(type), mVertexesCount(s) {}
+	VertexBufferType getType(void) { return m_type; }
+protected:
+	VertexBufferType m_type;
+	glm::vec3				*mVertexes;
+	glm::vec3				*mNormals;
+	glm::vec2				*mTexCoords;
+	glm::vec3				*mColors;
+	unsigned int mVertexesCount;
 };
 
-class CPUVertexBuffer {
+class GLVertexBuffer : VertexBuffer {
 public:
-	CPUVertexBuffer() {}
-	glm::vec3	*m_vertexPointer;
-};
+	GLVertexBuffer(unsigned int size);
 
-class GLVertexBuffer {
-public:
-	GLVertexBuffer() {}
+	void setVertexes(glm::vec3 *data);
+	void setNormals(glm::vec3 *data);
+	void setTextCoords(glm::uvec2 *data);
+	void setColors(glm::vec3 *data);
 
-	GLint getVBO(void) const;
+	enum VertexAttribute {
+		VERTEX_ATTR_POSITION,
+		VERTEX_ATTR_NORMAL,
+		VERTEX_ATTR_TEX_COORDS,
+		VERTEX_ATTR_COLOR,
+		VERTEX_ATTR_LAST
+	};
+
+	bool bind(VertexAttribute attr);
+	void unbind(void);
+
+private:
+	GLuint mVBO[VERTEX_ATTR_LAST];
 };
 
 #endif
