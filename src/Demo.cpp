91e8d8f119ce860dc1f87adc88c924c8d8b87ec9
Lukasz Stanislawski <lukasz.stanislawski@gmail.com>
2014-02-06 20:53:44 +0100

    tab => 4 spaces

--- src/Demo.cpp
+++ src/Demo.cpp
@@ -6,54 +6,54 @@ using namespace std;
 class Demo : public GLUTApplication
 {
 public:
-	Demo(int argc, char **argv);
-	~Demo(void) {}
-	void onKeyboard(unsigned char k, int x, int y);
-	void onMouseClick(int key, int state, int x, int y);
-	void onMouseMove(int x, int y);
-	void onMenuSelected(string &s);
+    Demo(int argc, char **argv);
+    ~Demo(void) {}
+    void onKeyboard(unsigned char k, int x, int y);
+    void onMouseClick(int key, int state, int x, int y);
+    void onMouseMove(int x, int y);
+    void onMenuSelected(string &s);
 };
 
 Demo::Demo(int argc, char **argv) :
-	GLUTApplication(argc, argv, "DemoApp", 400, 400)
+    GLUTApplication(argc, argv, "DemoApp", 400, 400)
 {
-	initialize();
+    initialize();
 
-	int id1 = addMenu("Main");
-	addMenuEntry(id1, "Hello1");
-	addMenuEntry(id1, "Hello2");
+    int id1 = addMenu("Main");
+    addMenuEntry(id1, "Hello1");
+    addMenuEntry(id1, "Hello2");
 
-	int id2 = addMenu("SubMenu", id1);
-	addMenuEntry(id2, "Hello3");
-	addMenuEntry(id2, "Hello4");
+    int id2 = addMenu("SubMenu", id1);
+    addMenuEntry(id2, "Hello3");
+    addMenuEntry(id2, "Hello4");
 
-	attachMenu(id1, RIGHT_BUTTON);
+    attachMenu(id1, RIGHT_BUTTON);
 }
 
 void Demo::onKeyboard(unsigned char k, int x, int y)
 {
-	cout << "OnKeyboard : " << k << endl;
-	if (k == 'q')
-		shutdown();
+    cout << "OnKeyboard : " << k << endl;
+    if (k == 'q')
+        shutdown();
 }
 
 void Demo::onMouseClick(int key, int state, int x, int y)
 {
-	cout << "OnClick" << endl;
+    cout << "OnClick" << endl;
 }
 
 void Demo::onMouseMove(int x, int y)
 {
-	cout << "OnMouseMove" << endl;
+    cout << "OnMouseMove" << endl;
 }
 
 void Demo::onMenuSelected(string &s)
 {
-	cout << s << endl;
+    cout << s << endl;
 }
 
 int main(int argc, char **argv)
 {
-	Demo demo(argc, argv);
-	demo.run();
+    Demo demo(argc, argv);
+    demo.run();
 }
--- src/renderer/Shader.cpp
+++ src/renderer/Shader.cpp
@@ -4,133 +4,133 @@
 using namespace glm;
 
 Shader::~Shader(void) {
-	if (iFragmentShader) glDeleteShader(iFragmentShader);
-	if (iVertexShader) glDeleteShader(iVertexShader);
-	if (iGeomShader) glDeleteShader(iGeomShader);
-	if (iProgram) glDeleteProgram(iProgram);
+    if (iFragmentShader) glDeleteShader(iFragmentShader);
+    if (iVertexShader) glDeleteShader(iVertexShader);
+    if (iGeomShader) glDeleteShader(iGeomShader);
+    if (iProgram) glDeleteProgram(iProgram);
 }
 
 void Shader::setShaderSource(GLenum type, const char *src)
 {
-	switch(type) {
-		case GL_VERTEX_SHADER:
-			pVertexSrc = src;
-			break;
-		case GL_FRAGMENT_SHADER:
-			pFragSrc = src;
-			break;
-		case GL_GEOMETRY_SHADER:
-			pGeomSrc = src;
-			break;
-		default:
-			ERR("Invalid shader type.");
-	}
+    switch(type) {
+        case GL_VERTEX_SHADER:
+            pVertexSrc = src;
+            break;
+        case GL_FRAGMENT_SHADER:
+            pFragSrc = src;
+            break;
+        case GL_GEOMETRY_SHADER:
+            pGeomSrc = src;
+            break;
+        default:
+            ERR("Invalid shader type.");
+    }
 }
 
 GLuint Shader::createShader(GLenum type, const char *src)
 {
-	GLuint shader;
-	GLint status, len;
-	char *log;
+    GLuint shader;
+    GLint status, len;
+    char *log;
 
-	shader = glCreateShader(type);
-	if (shader == 0) {
-		ERR("glCreateShader failed.");
-		return 0;
-	}
-	
-	glShaderSource(shader, 1, (const GLchar**)&src, NULL);
-	glCompileShader(shader);
-	glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
+    shader = glCreateShader(type);
+    if (shader == 0) {
+        ERR("glCreateShader failed.");
+        return 0;
+    }
+    
+    glShaderSource(shader, 1, (const GLchar**)&src, NULL);
+    glCompileShader(shader);
+    glGetShaderiv(shader, GL_COMPILE_STATUS, &status);
 
-	if (status != GL_TRUE) {
-		ERR("Shader compilation failed.");
-		glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
-		log = new char[len];
-		glGetShaderInfoLog(shader, len, &len, log);
-		ERR("%s", log);
-		delete log;
-		glDeleteShader(shader);
-		return 0;
-	}
+    if (status != GL_TRUE) {
+        ERR("Shader compilation failed.");
+        glGetShaderiv(shader, GL_INFO_LOG_LENGTH, &len);
+        log = new char[len];
+        glGetShaderInfoLog(shader, len, &len, log);
+        ERR("%s", log);
+        delete log;
+        glDeleteShader(shader);
+        return 0;
+    }
 
-	return shader;
+    return shader;
 }
 
 bool Shader::compileAndLink(void)
 {
-	GLint status;
-	if (!pVertexSrc || !pFragSrc) {
-		ERR("Vertex and Fragment shader are required.");
-		return false;
-	}
-	iFragmentShader = createShader(GL_FRAGMENT_SHADER, pFragSrc);
-	iVertexShader = createShader(GL_VERTEX_SHADER, pVertexSrc);
+    GLint status;
+    if (!pVertexSrc || !pFragSrc) {
+        ERR("Vertex and Fragment shader are required.");
+        return false;
+    }
+    iFragmentShader = createShader(GL_FRAGMENT_SHADER, pFragSrc);
+    iVertexShader = createShader(GL_VERTEX_SHADER, pVertexSrc);
 
-	if (!iVertexShader || !iFragmentShader)
-		goto on_error;
+    if (!iVertexShader || !iFragmentShader)
+        goto on_error;
 
-	if (pGeomSrc) {
-		iGeomShader = createShader(GL_GEOMETRY_SHADER, pGeomSrc);
-		if (!iGeomShader)
-			goto on_error;
-	}
+    if (pGeomSrc) {
+        iGeomShader = createShader(GL_GEOMETRY_SHADER, pGeomSrc);
+        if (!iGeomShader)
+            goto on_error;
+    }
 
-	iProgram = glCreateProgram();
-	if (!iProgram)
-		goto on_error;
+    iProgram = glCreateProgram();
+    if (!iProgram)
+        goto on_error;
 
-	glAttachShader(iProgram, iVertexShader);
-	glAttachShader(iProgram, iFragmentShader);
-	if (iGeomShader)
-		glAttachShader(iProgram, iGeomShader);
+    glAttachShader(iProgram, iVertexShader);
+    glAttachShader(iProgram, iFragmentShader);
+    if (iGeomShader)
+        glAttachShader(iProgram, iGeomShader);
 
-	glLinkProgram(iProgram);
+    glLinkProgram(iProgram);
 
-	glGetProgramiv(iProgram, GL_LINK_STATUS, &status);
-	if (status != GL_TRUE) {
-		ERR("glLinkProgram failed.");
-		goto on_error;
-	}
-	DBG("Shader compile & link successed.");
-	return true;
+    glGetProgramiv(iProgram, GL_LINK_STATUS, &status);
+    if (status != GL_TRUE) {
+        ERR("glLinkProgram failed.");
+        goto on_error;
+    }
+    DBG("Shader compile & link successed.");
+    return true;
 
 on_error:
-	if (iFragmentShader) glDeleteShader(iFragmentShader);
-	if (iVertexShader) glDeleteShader(iVertexShader);
-	if (iGeomShader) glDeleteShader(iGeomShader);
-	if (iProgram) glDeleteProgram(iProgram);
-	return false;
+    if (iFragmentShader) glDeleteShader(iFragmentShader);
+    if (iVertexShader) glDeleteShader(iVertexShader);
+    if (iGeomShader) glDeleteShader(iGeomShader);
+    if (iProgram) glDeleteProgram(iProgram);
+    return false;
 }
 
 void Shader::useShader(void)
 {
-	if (iProgram)
-		glUseProgram(iProgram);
-	else 
-		ERR("Program not compiled and linked.");
+    if (iProgram)
+        glUseProgram(iProgram);
+    else 
+        ERR("Program not compiled and linked.");
 }
 
 void Shader::setUniform(const char *name, const glm::vec3 *v)
 {
-	if (!iProgram)
-		return;
-	GLint k = glGetUniformLocation(iProgram, name);
-	if (k == -1) {
-		ERR("Invalid uniform name: %s", name);
-		return;
-	}
-	glUniform3fv(k, 1, glm::value_ptr(*v));
+    if (!iProgram)
+        return;
+    GLint k = glGetUniformLocation(iProgram, name);
+    if (k == -1) {
+        ERR("Invalid uniform name: %s", name);
+        return;
+    }
+    glUniform3fv(k, 1, glm::value_ptr(*v));
 }
 
 void Shader::setUniform(const char *name, const glm::mat4 *v)
 {
-	if (!iProgram)
-		return;
-	GLint k = glGetUniformLocation(iProgram, name);
-	if (k == -1) {
-		ERR("Invalid uniform name: %s", name);
-		return;
-	}
-	glUniformMatrix4fv(k, 1, false, glm::value_ptr(*v));
+    if (!iProgram)
+        return;
+    GLint k = glGetUniformLocation(iProgram, name);
+    if (k == -1) {
+        ERR("Invalid uniform name: %s", name);
+        return;
+    }
+    glUniformMatrix4fv(k, 1, false, glm::value_ptr(*v));
 }
