4a167c912ebfb46fb3b8cf0327f4acf5a1d3ba39
Lukasz Stanislawski <lukasz.stanislawski@gmail.com>
2014-02-06 19:40:24 +0100

    SoftBody constructor/descrutor implemented.
    Tabs => spaces

--- src/engine/core/SoftBody.cpp
+++ src/engine/core/SoftBody.cpp
@@ -1,5 +1,38 @@
 #include "SoftBody.h"
 
+using namespace glm;
+
+SoftBody::SoftBody(float_t mass, float_t springness, float_t damping,
+		vec3 *particles, unsigned int particles_count,
+	 	uvec2 *links_indexes, unsigned int links_count,
+	 	uvec4 *volumes_indexes, unsigned int volumes_count)
+	:
+		mVertexBuffer(0)
+{
+	mMassInv = 1.0/mass;
+	mSpringiness = springness;
+	mDamping = damping;
+	mParticles.resize(particles_count);
+	mVelocities.resize(particles_count);
+	mForces.resize(particles_count);
+	mLinks.resize(links_count);
+	mVolumes.resize(volumes_count);
+
+	for(unsigned int i = 0; i < particles_count; i++)
+		mParticles.push_back(particles[i]);
+	for(unsigned int i = 0; i < links_count; i++) {
+		LinkConstraint lnk;
+		lnk.index = links_indexes[i];
+		lnk.restLength = length(mParticles[lnk.index[0]] - mParticles[lnk.index[1]]);
+		mLinks.push_back(lnk);
+	}
+}
+
+SoftBody::~SoftBody(void)
+{
+	if (mVertexBuffer) delete mVertexBuffer;
+}
+
 void SoftBody::createGLVertexBuffer(void)
 {
 	GLVertexBuffer *buf = new GLVertexBuffer(mParticles.size());
--- src/engine/core/SoftBody.h
+++ src/engine/core/SoftBody.h
@@ -13,73 +13,76 @@ class CUDASoftBodySolver;
 ///gclass CollisionBody {
 ///g
 ///gpublic:
-///g	enum {
-///g		PLANE,
-///g		SPHERE,
-///g	} BoundingType;
+///g    enum {
+///g        PLANE,
+///g        SPHERE,
+///g    } BoundingType;
 ///g
-///g	CollisionBody(BoundingType &bt);
-///g	~CollisionBody(void);
+///g    CollisionBody(BoundingType &bt);
+///g    ~CollisionBody(void);
 ///g
-///g	BoundingType getBoundingType(void);
-///g	bool 		 checkCollision(CollisionBody &body) = 0;
-///g	glm::vec3	 collisionPoint(CollisionBody &body) = 0;
+///g    BoundingType getBoundingType(void);
+///g    bool          checkCollision(CollisionBody &body) = 0;
+///g    glm::vec3     collisionPoint(CollisionBody &body) = 0;
 ///g
-///g	union {
-///g		struct {
-///g			glm::vec3 origin;
-///g			glm::vec3 normal;
-///g		} plane;
-///g		struct {
-///g			glm::vec3 origin;
-///g			glm::float_t radius;
-///g		} sphere,
-///g	} data;
-///g	glm::float_t offset;
+///g    union {
+///g        struct {
+///g            glm::vec3 origin;
+///g            glm::vec3 normal;
+///g        } plane;
+///g        struct {
+///g            glm::vec3 origin;
+///g            glm::float_t radius;
+///g        } sphere,
+///g    } data;
+///g    glm::float_t offset;
 ///g};
 ///g
 ///g
 //
 
 struct LinkConstraint {
-	glm::uvec2 		index;
-	glm::float_t	restLength;
+    glm::uvec2          index;
+    glm::float_t        restLength;
 };
 
 struct VolumeConstraint {
-	glm::uvec4 		index;
-	glm::float_t 	restLength;
+    glm::uvec4          index;
+    glm::float_t        restLength;
 };
 
-typedef std::vector<glm::vec3>			vec3Array_t;
-typedef std::vector<LinkConstraint>		linksArray_t;
-typedef std::vector<VolumeConstraint>	volumeArray_t;
+typedef std::vector<glm::vec3>              vec3Array_t;
+typedef std::vector<LinkConstraint>         linksArray_t;
+typedef std::vector<VolumeConstraint>       volumeArray_t;
 
 class SoftBody {
 public:
-	SoftBody(glm::vec3 *particles, unsigned int particles_count,
-			 glm::uvec2 *links_indexes, unsigned int links_count,
-			 glm::uvec4 *volumes_indexes, unsigned int volumes_count);
-	~SoftBody(void);
+    SoftBody(glm::float_t mass, glm::float_t springness, glm::float_t damping,
+             glm::vec3 *particles, unsigned int particles_count,
+             glm::uvec2 *links_indexes, unsigned int links_count,
+             glm::uvec4 *volumes_indexes, unsigned int volumes_count);
+    ~SoftBody(void);
 
-	void					initVertexBuffer(VertexBuffer::VertexBufferType);
-	const VertexBuffer		*getVertexBuffer(void);
+    void                        initVertexBuffer(VertexBuffer::VertexBufferType);
+    const VertexBuffer          *getVertexBuffer(void);
 private:
-	void createGLVertexBuffer(void);
+    void createGLVertexBuffer(void);
 
-	vec3Array_t				 	mParticles;
-	vec3Array_t					mVelocities;
-	vec3Array_t					mForces;
+    vec3Array_t                 mParticles;
+    vec3Array_t                 mVelocities;
+    vec3Array_t                 mForces;
 
-	// inverted mass of every particle
-	float_t						mMassInv;
+    // inverted mass of every particle
+    glm::float_t                mMassInv;
+    glm::float_t                mDamping;
+    glm::float_t                mSpringiness;
 
-	// constraints in soft body
-	linksArray_t			 	mLinks;
-	volumeArray_t				mVolumes;
+    // constraints in soft body
+    linksArray_t                mLinks;
+    volumeArray_t               mVolumes;
 
-	// buffer object
-	VertexBuffer				*mVertexBuffer;
+    // buffer object
+    VertexBuffer                *mVertexBuffer;
 
 friend class CUDASoftBodySolver;
 };
