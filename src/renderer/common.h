b3b6eec9f9fb147b26642d8573f9ee834f410aa9
Lukasz Stanislawski <lukasz.stanislawski@gmail.com>
2014-02-06 21:26:20 +0100

    common.h is now shared

--- src/renderer/common.h
+++
@@ -1,8 +0,0 @@
-#define VAR(v,w) __typeof(w) v=w
-
-#define FOREACH(it,c) for(VAR(it,(c)->begin());it!=(c)->end();++it)
-#define FOREACH_R(it,c) for(VAR(it,(c).begin());it!=(c).end();++it)
-
-#define DBG(fmt, ...) fprintf(stderr, "\033[22;32m:%s:%d:" fmt, __func__, __LINE__, ##__VA_ARGS__)
-#define ERR(fmt, ...) fprintf(stderr, "\033[01;31m:%s:%d:" fmt, __func__, __LINE__, ##__VA_ARGS__)
-#define WRN(fmt, ...) fprintf(stderr, "\033[01;33m:%s:%d:" fmt, __func__, __LINE__, ##__VA_ARGS__)
