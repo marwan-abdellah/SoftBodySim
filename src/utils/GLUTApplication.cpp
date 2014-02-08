e706225a77f1dc8fd66d0f9143f719ab5f0a1f9b
Lukasz Stanislawski <lukasz.stanislawski@gmail.com>
2014-02-08 14:27:52 +0100

    styling: tab => 4 spaces

--- src/utils/GLUTApplication.cpp
+++ src/utils/GLUTApplication.cpp
@@ -8,151 +8,151 @@ using namespace std;
 
 void GLUTApplication::onDisplayHelper(void)
 {
-	if (active_instance)
-		active_instance->onDisplay();
-	glutSwapBuffers();
-	glutPostRedisplay();
+    if (active_instance)
+        active_instance->onDisplay();
+    glutSwapBuffers();
+    glutPostRedisplay();
 }
 
 void GLUTApplication::onIdleHelper()
 {
-	glutPostRedisplay();
+    glutPostRedisplay();
 }
 
 void GLUTApplication::onKeyboardHelper(unsigned char k, int x, int y)
 {
-	if (active_instance)
-		active_instance->onKeyboard(k, x, y);
+    if (active_instance)
+        active_instance->onKeyboard(k, x, y);
 }
 
 void GLUTApplication::onKeyboardUpHelper(unsigned char k, int x, int y)
 {
-	if (active_instance)
-		active_instance->onKeyboardUp(k, x, y);
+    if (active_instance)
+        active_instance->onKeyboardUp(k, x, y);
 }
 
 void GLUTApplication::onMouseClickHelper(int key, int state, int x, int y)
 {
-	ButtonType type;
-	ButtonState st;
-	switch (key) {
-		case GLUT_LEFT_BUTTON:
-			type = LEFT_BUTTON;
-			break;
-		case GLUT_MIDDLE_BUTTON:
-			type = MIDDLE_BUTTON;
-			break;
-		case GLUT_RIGHT_BUTTON:
-			type = RIGHT_BUTTON;
-			break;
-	}
+    ButtonType type;
+    ButtonState st;
+    switch (key) {
+        case GLUT_LEFT_BUTTON:
+            type = LEFT_BUTTON;
+            break;
+        case GLUT_MIDDLE_BUTTON:
+            type = MIDDLE_BUTTON;
+            break;
+        case GLUT_RIGHT_BUTTON:
+            type = RIGHT_BUTTON;
+            break;
+    }
 
-	switch (state) {
-		case GLUT_DOWN:
-			st = PRESSED;
-			break;
-		case GLUT_UP:
-			st = RELEASED;
-			break;
-	}
+    switch (state) {
+        case GLUT_DOWN:
+            st = PRESSED;
+            break;
+        case GLUT_UP:
+            st = RELEASED;
+            break;
+    }
 
-	if (active_instance)
-		active_instance->onMouseClick(type, st, x, y);
+    if (active_instance)
+        active_instance->onMouseClick(type, st, x, y);
 }
 
 void GLUTApplication::onMouseMoveHelper(int x, int y)
 {
-	if (active_instance)
-		active_instance->onMouseMove(x, y);
+    if (active_instance)
+        active_instance->onMouseMove(x, y);
 }
 
 void GLUTApplication::onMenuSelectedHelper(int id)
 {
-	if (active_instance)
-		active_instance->onMenuSelected(active_instance->m_menuKeys[id]);
+    if (active_instance)
+        active_instance->onMenuSelected(active_instance->m_menuKeys[id]);
 }
 
 GLUTApplication::GLUTApplication(int argc, char **argv, const char *title, int width, int height) :
-	m_argc(argc),
-	m_argv(argv),
-	m_title(title),
-	m_width(width),
-	m_height(height)
+    m_argc(argc),
+    m_argv(argv),
+    m_title(title),
+    m_width(width),
+    m_height(height)
 {
 }
 
 GLUTApplication::~GLUTApplication()
 {
-	shutdown();
+    shutdown();
 }
 
 void GLUTApplication::attachMenu(int menuId, ButtonType button)
 {
-	glutSetMenu(menuId);
-	int bid;
-	switch (button) {
-		case LEFT_BUTTON:
-			bid = GLUT_LEFT_BUTTON;
-			break;
-		case RIGHT_BUTTON:
-			bid = GLUT_RIGHT_BUTTON;
-			break;
-		case MIDDLE_BUTTON:
-			bid = GLUT_MIDDLE_BUTTON;
-	}
-	glutAttachMenu(bid);
+    glutSetMenu(menuId);
+    int bid;
+    switch (button) {
+        case LEFT_BUTTON:
+            bid = GLUT_LEFT_BUTTON;
+            break;
+        case RIGHT_BUTTON:
+            bid = GLUT_RIGHT_BUTTON;
+            break;
+        case MIDDLE_BUTTON:
+            bid = GLUT_MIDDLE_BUTTON;
+    }
+    glutAttachMenu(bid);
 }
 
 void GLUTApplication::addMenuEntry(int menuId, string entry)
 {
-	glutSetMenu(menuId);
-	glutAddMenuEntry(entry.c_str(), m_menuKeys.size());
-	m_menuKeys.push_back(entry);
+    glutSetMenu(menuId);
+    glutAddMenuEntry(entry.c_str(), m_menuKeys.size());
+    m_menuKeys.push_back(entry);
 }
 
 int GLUTApplication::addMenu(string entry, int parentMenuId)
 {
-	int id;
-	id = glutCreateMenu(GLUTApplication::onMenuSelectedHelper);
+    int id;
+    id = glutCreateMenu(GLUTApplication::onMenuSelectedHelper);
 
-	if (parentMenuId != -1) {
-		glutSetMenu(parentMenuId);
-		glutAddSubMenu(entry.c_str(), id);
-	}
+    if (parentMenuId != -1) {
+        glutSetMenu(parentMenuId);
+        glutAddSubMenu(entry.c_str(), id);
+    }
 
-	return id;
+    return id;
 }
 
 void GLUTApplication::initialize(void)
 {
-	if (!active_instance) {
-		glutInit(&m_argc, m_argv);
-		glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
-		glutInitWindowSize(m_width, m_height);
-		glutCreateWindow(m_title.c_str());
+    if (!active_instance) {
+        glutInit(&m_argc, m_argv);
+        glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGBA | GLUT_DEPTH);
+        glutInitWindowSize(m_width, m_height);
+        glutCreateWindow(m_title.c_str());
 
-		glutKeyboardFunc(GLUTApplication::onKeyboardHelper);
-		glutKeyboardUpFunc(GLUTApplication::onKeyboardUpHelper);
-		glutMouseFunc(GLUTApplication::onMouseClickHelper);
-		glutMotionFunc(GLUTApplication::onMouseMoveHelper);
-		glutIdleFunc(GLUTApplication::onIdleHelper);
-		glutDisplayFunc(GLUTApplication::onDisplayHelper);
+        glutKeyboardFunc(GLUTApplication::onKeyboardHelper);
+        glutKeyboardUpFunc(GLUTApplication::onKeyboardUpHelper);
+        glutMouseFunc(GLUTApplication::onMouseClickHelper);
+        glutMotionFunc(GLUTApplication::onMouseMoveHelper);
+        glutIdleFunc(GLUTApplication::onIdleHelper);
+        glutDisplayFunc(GLUTApplication::onDisplayHelper);
 
-		active_instance = this;
-	}
+        active_instance = this;
+    }
 }
 
 void GLUTApplication::shutdown(void)
 {
-	if (active_instance != this)
-		return;
-	glutLeaveMainLoop();
-	active_instance = NULL;
+    if (active_instance != this)
+        return;
+    glutLeaveMainLoop();
+    active_instance = NULL;
 }
 
 void GLUTApplication::run(void)
 {
-	if (active_instance != this)
-		return;
-	glutMainLoop();
+    if (active_instance != this)
+        return;
+    glutMainLoop();
 }
--- src/utils/GLUTApplication.h
+++ src/utils/GLUTApplication.h
@@ -7,49 +7,49 @@
 class GLUTApplication
 {
 public:
-	virtual ~GLUTApplication();
-	GLUTApplication(int argc, char **argv, const char *title, int width, int height);
-	void initialize(void);
-	void shutdown(void);
-	void run(void);
+    virtual ~GLUTApplication();
+    GLUTApplication(int argc, char **argv, const char *title, int width, int height);
+    void initialize(void);
+    void shutdown(void);
+    void run(void);
 
-	enum ButtonType {
-		RIGHT_BUTTON,
-		MIDDLE_BUTTON,
-		LEFT_BUTTON
-	};
+    enum ButtonType {
+        RIGHT_BUTTON,
+        MIDDLE_BUTTON,
+        LEFT_BUTTON
+    };
 
-	enum ButtonState {
-		PRESSED,
-		RELEASED
-	};
+    enum ButtonState {
+        PRESSED,
+        RELEASED
+    };
 
-	void attachMenu(int menuId, ButtonType button);
-	void addMenuEntry(int menuId, std::string entry);
-	int addMenu(std::string entry, int parentMenuId=-1);
+    void attachMenu(int menuId, ButtonType button);
+    void addMenuEntry(int menuId, std::string entry);
+    int addMenu(std::string entry, int parentMenuId=-1);
 
-	virtual void onDisplay(void) {}
-	virtual void onKeyboardUp(unsigned char k, int x, int y) {}
-	virtual void onKeyboard(unsigned char k, int x, int y) {}
-	virtual void onMouseClick(ButtonType btn, ButtonState state, int x, int y) {}
-	virtual void onMouseMove(int x, int y) {}
-	virtual void onMenuSelected(std::string &) {}
+    virtual void onDisplay(void) {}
+    virtual void onKeyboardUp(unsigned char k, int x, int y) {}
+    virtual void onKeyboard(unsigned char k, int x, int y) {}
+    virtual void onMouseClick(ButtonType btn, ButtonState state, int x, int y) {}
+    virtual void onMouseMove(int x, int y) {}
+    virtual void onMenuSelected(std::string &) {}
 protected:
-	int m_argc;
-	char **m_argv;
+    int m_argc;
+    char **m_argv;
 private:
-	std::string m_title;
-	int m_width, m_height;
-	std::vector<std::string> m_menuKeys;
-	GLUTApplication(const GLUTApplication&) {}
-	static GLUTApplication *active_instance;
-	static void onKeyboardHelper(unsigned char, int, int);
-	static void onKeyboardUpHelper(unsigned char, int x, int y);
-	static void onMouseClickHelper(int, int, int, int);
-	static void onMouseMoveHelper(int, int);
-	static void onIdleHelper(void);
-	static void onDisplayHelper(void);
-	static void onMenuSelectedHelper(int id);
+    std::string m_title;
+    int m_width, m_height;
+    std::vector<std::string> m_menuKeys;
+    GLUTApplication(const GLUTApplication&) {}
+    static GLUTApplication *active_instance;
+    static void onKeyboardHelper(unsigned char, int, int);
+    static void onKeyboardUpHelper(unsigned char, int x, int y);
+    static void onMouseClickHelper(int, int, int, int);
+    static void onMouseMoveHelper(int, int);
+    static void onIdleHelper(void);
+    static void onDisplayHelper(void);
+    static void onMenuSelectedHelper(int id);
 };
 
 #endif
