32ec5fb8f3e7b3125dc0fd9e707e7f79bd07a6ff
Lukasz Stanislawski <lukasz.stanislawski@gmail.com>
2014-05-23 23:11:32 +0200

    demo: style fix [spaces->tabs]

--- src/demos/Demo.cpp
+++ src/demos/Demo.cpp
@@ -21,43 +21,43 @@ const int height = 600;
 class Demo : public GLFWApplication
 {
 public:
-    Demo(int argc, char **argv);
-    ~Demo(void);
-    void OnKeyboard(int k, int action);
-    void OnMouseClick(int type, int state, int x, int y);
-    void OnMouseMove(int x, int y);
-    void OnRender(void);
-    void OnUpdate(double dt);
+	Demo(int argc, char **argv);
+	~Demo(void);
+	void OnKeyboard(int k, int action);
+	void OnMouseClick(int type, int state, int x, int y);
+	void OnMouseMove(int x, int y);
+	void OnRender(void);
+	void OnUpdate(double dt);
 private:
 	Body *floor;
-    SoftBodyRenderer    renderer;
-    softbodyList_t     mSoftBodies;
-    int mMouseLastX;
-    int mMouseLastY;
-    bool mMousePressed;
-    Camera mCamera;
-    CUDASoftBodySolver mSolver;
+	SoftBodyRenderer    renderer;
+	softbodyList_t     mSoftBodies;
+	int mMouseLastX;
+	int mMouseLastY;
+	bool mMousePressed;
+	Camera mCamera;
+	CUDASoftBodySolver mSolver;
 	int	mEnginUpdateTime;
 };
 
 Demo::Demo(int argc, char **argv) :
-    GLFWApplication("DemoApp", width, height),
-    mCamera(vec3(0,0,8), vec3(0,0,0), vec3(0,1,0)),
+	GLFWApplication("DemoApp", width, height),
+	mCamera(vec3(0,0,8), vec3(0,0,0), vec3(0,1,0)),
 	mSolver()
 {
 	MeshData *md = MeshData::CreateFromObj("src/demos/cube.obj");
 	MeshData *md2 = MeshData::CreatePlane(3.0, 3.0, 2, 2);
 
-    SoftBody *b = new SoftBody(1.0f, 0.1f, 1.0f, md);
-    mSoftBodies.push_back(b);
+	SoftBody *b = new SoftBody(1.0f, 0.1f, 1.0f, md);
+	mSoftBodies.push_back(b);
 
 	floor = new Body(md2);
 
-    renderer.initialize(width, height);
-    renderer.setRenderMethod(SB_RENDER_PARTICLES);
+	renderer.initialize(width, height);
+	renderer.setRenderMethod(SB_RENDER_PARTICLES);
 
 	mSolver.addSoftBodies(mSoftBodies);
-    mSolver.initialize();
+	mSolver.initialize();
 
 	delete md;
 	delete md2;
@@ -65,13 +65,10 @@ Demo::Demo(int argc, char **argv) :
 
 Demo::~Demo(void)
 {
-    FOREACH(b, &mSoftBodies)
-        delete *b;
+	FOREACH(b, &mSoftBodies)
+		delete *b;
 	delete floor;
 }
-
-#define ENGINE_TIME_STEP 15
-#define DIFF_MAX 200
 
 void Demo::OnUpdate(double dt)
 {
@@ -91,75 +88,75 @@ void Demo::OnRender(void)
 
 void Demo::OnKeyboard(int key, int action)
 {
-    float angle = 2.0f;
-    float delta = 0.1f;
+	float angle = 2.0f;
+	float delta = 0.1f;
 
 	if (action == GLFW_RELEASE) return;
 
-    if (key == GLFW_KEY_W)
-        mCamera.moveUp(angle);
-    if (key == GLFW_KEY_D)
-        mCamera.moveDown(angle);
-    if (key == GLFW_KEY_Z)
-        mCamera.moveIn(delta);
-    if (key == GLFW_KEY_X)
-        mCamera.moveOut(delta);
-    if (key == GLFW_KEY_M)
-    {
-        switch (renderer.getRenderMethod()) {
-            case SB_RENDER_PARTICLES:
-                renderer.setRenderMethod(SB_RENDER_FACES);
-                break;
-            case SB_RENDER_FACES:
-                renderer.setRenderMethod(SB_RENDER_PARTICLES);
-                break;
-        }
-    }
-    if (key == GLFW_KEY_A)
-        mCamera.moveRight(angle);
-    else if (key == GLFW_KEY_D)
-        mCamera.moveLeft(angle);
+	if (key == GLFW_KEY_W)
+		mCamera.moveUp(angle);
+	if (key == GLFW_KEY_D)
+		mCamera.moveDown(angle);
+	if (key == GLFW_KEY_Z)
+		mCamera.moveIn(delta);
+	if (key == GLFW_KEY_X)
+		mCamera.moveOut(delta);
+	if (key == GLFW_KEY_M)
+	{
+		switch (renderer.getRenderMethod()) {
+			case SB_RENDER_PARTICLES:
+				renderer.setRenderMethod(SB_RENDER_FACES);
+				break;
+			case SB_RENDER_FACES:
+				renderer.setRenderMethod(SB_RENDER_PARTICLES);
+			break;
+		}
+	}
+	if (key == GLFW_KEY_A)
+		mCamera.moveRight(angle);
+	else if (key == GLFW_KEY_D)
+		mCamera.moveLeft(angle);
 }
 
 void Demo::OnMouseClick(int type, int state, int x, int y)
 {
-    if (type != GLFW_MOUSE_BUTTON_1)
-        return;
+	if (type != GLFW_MOUSE_BUTTON_1)
+		return;
 
-    if (state == GLFW_PRESS) {
-        mMouseLastX = x;
-        mMouseLastY = y;
-        mMousePressed = true;
-    }
-    else if (state == GLFW_RELEASE)
-        mMousePressed = false;
+	if (state == GLFW_PRESS) {
+		mMouseLastX = x;
+		mMouseLastY = y;
+		mMousePressed = true;
+	}
+	else if (state == GLFW_RELEASE)
+		mMousePressed = false;
 }
 
 void Demo::OnMouseMove(int x, int y)
 {
-    if (!mMousePressed)
-        return;
+	if (!mMousePressed)
+		return;
 
-    static float angle = 0.2f;
-    int dx = x - mMouseLastX;
-    int dy = y - mMouseLastY;
+	static float angle = 0.2f;
+	int dx = x - mMouseLastX;
+	int dy = y - mMouseLastY;
 
-    vec3 np;
-    if (dx > 0)
-        mCamera.moveRight(angle * dx);
-    else
-        mCamera.moveLeft(-angle * dx);
-    if (dy > 0)
-        mCamera.moveUp(angle * dy);
-    else
-        mCamera.moveDown(-angle * dy);
+	vec3 np;
+	if (dx > 0)
+		mCamera.moveRight(angle * dx);
+	else
+		mCamera.moveLeft(-angle * dx);
+	if (dy > 0)
+		mCamera.moveUp(angle * dy);
+	else
+		mCamera.moveDown(-angle * dy);
 
-    mMouseLastX = x;
-    mMouseLastY = y;
+	mMouseLastX = x;
+	mMouseLastY = y;
 }
 
 int main(int argc, char **argv)
 {
-    Demo demo(argc, argv);
-    demo.MainLoop(0.02);
+	Demo demo(argc, argv);
+	demo.MainLoop(0.02);
 }
